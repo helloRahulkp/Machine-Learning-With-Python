ada66b66523594d8cf681b9a102c915f3d98f704
helloRahulkp <hello.rahulkp@gmail.com>
2025-09-14 14:55:52 +0530

    Evaluating Classification Models

---
+++ EvaluatingClassificationModels.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the Breast Cancer data set
+data = load_breast_cancer()
+X, y = data.data, data.target
+labels = data.target_names
+feature_names = data.feature_names
+
+# Print the description of the Breast Cancer data set
+print(data.DESCR)
+print("Target names:")
+print(data.target_names)
+
+# Standardize the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Add some noise
+# Add Gaussian noise to the data set
+np.random.seed(42)  # For reproducibility
+noise_factor = 0.5 # Adjust this to control the amount of noise
+X_noisy = X_scaled + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=X.shape)
+
+# Load the original and noisy data sets into a DataFrame for comparison and visualization
+df = pd.DataFrame(X_scaled, columns=feature_names)
+df_noisy = pd.DataFrame(X_noisy, columns=feature_names)
+
+# Display the first few rows of the standardized original and noisy data sets for comparison
+print("Original Data (First 5 rows):")
+df.head()
+
+print("\nNoisy Data (First 5 rows):")
+df_noisy.head()
+
+# Visualizing the noise content.
+plt.figure(figsize=(12, 6))
+
+# Original Feature Distribution (Noise-Free)
+plt.subplot(1, 2, 1)
+plt.hist(df[feature_names[5]], bins=20, alpha=0.7, color='blue', label='Original')
+plt.title('Original Feature Distribution')
+plt.xlabel(feature_names[5])
+plt.ylabel('Frequency')
+
+# Noisy Feature Distribution
+plt.subplot(1, 2, 2)
+plt.hist(df_noisy[feature_names[5]], bins=20, alpha=0.7, color='red', label='Noisy') 
+plt.title('Noisy Feature Distribution')
+plt.xlabel(feature_names[5])  
+plt.ylabel('Frequency')
+
+plt.tight_layout()  # Ensures proper spacing between subplots
+plt.show()
+
+# Plots
+plt.figure(figsize=(12, 6))
+plt.plot(df[feature_names[5]], label='Original',lw=3)
+plt.plot(df_noisy[feature_names[5]], '--',label='Noisy',)
+plt.title('Scaled feature comparison with and without noise')
+plt.xlabel(feature_names[5])
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+# Scatterplots
+plt.figure(figsize=(12, 6))
+plt.scatter(df[feature_names[5]], df_noisy[feature_names[5]],lw=5)
+plt.title('Scaled feature comparison with and without noise')
+plt.xlabel('Original Feature')
+plt.ylabel('Noisy Feature')
+plt.tight_layout()
+plt.show()
